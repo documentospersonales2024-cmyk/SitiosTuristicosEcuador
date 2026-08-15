1b66b8844028a6d045ef47ca8bba1b5d40b836b4
Mary Almeida <documentospersonales2024@gmail.com>
2026-08-15 00:13:27 -0500

    Avance 3 y 4

--- app/src/main/java/com/example/sitiosturisticosecuador/data/repository/TouristRepository.kt
+++ app/src/main/java/com/example/sitiosturisticosecuador/data/repository/TouristRepository.kt
@@ -20,6 +20,15 @@ class TouristRepository(private val dao: TouristPlaceDao) {
                 latitude = -0.0028,
                 longitude = -78.4553
             ),
+            TouristPlace(
+                id = "2",
+                name = "Volcán Quilotoa",
+                province = "Cotopaxi",
+                description = "Impresionante caldera volcánica con una laguna esmeralda.",
+                imageUrl = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/17/20/e7/0b/quilatoa-and-wild-flowers.jpg?w=1200&h=-1&s=1",
+                latitude = -0.8584,
+                longitude = -78.9038
+            )
         )
     }
 
